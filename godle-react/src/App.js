c05679690db063ea7252a42d64b8e068ce78916e
jenniferja <jenniferjaystar2@gmail.com>
2024-02-02 12:09:30 -0700

    cleaned up stuff

--- godle-react/src/App.js
+++ godle-react/src/App.js
@@ -9,16 +9,15 @@ const App = () => {
   return (
     <div>
       <div className="gradient-container"></div>
-        <div className="cursorCanvas">
-          
-        <Router>
-        <Routes>
-          <Route exact path="/" element={<RoutingPage page="Home"/>}/>
-          {//<Route path="/page-variable" exact={true} element={<RoutingPage page={"page-variable"}/>}/>
-          }
-        </Routes>
-      </Router>
-      <SparklingCursor />
+        <div className="cursorCanvas">   
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<RoutingPage page="Home"/>}/>
+              {//<Route path="/page-variable" exact={true} element={<RoutingPage page={"page-variable"}/>}/>
+              }
+            </Routes>
+          </Router>
+          <SparklingCursor />
         </div>
     </div>
     
