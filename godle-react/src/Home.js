7c915917a37296edea9af6233d7b6b3250e36098
jenniferja <jenniferjaystar2@gmail.com>
2024-02-04 14:50:16 -0700

    terms and services added Co-authored-by: Saaed-Darwish <Saaed-Darwish@users.noreply.github.com>

--- godle-react/src/Home.js
+++ godle-react/src/Home.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import Slider from './Slider';
 
 
 const Home = (props) => {
-    const handleSubmit = () => {
-        console.log("Form submitted!");
+    const [termsChecked, setTermsChecked] = useState(false);
+
+    const handleTermsChange = () => {
+        setTermsChecked(!termsChecked);
     };
 
+    const handleTermsClick = () => {
+        // Open the Google Docs link in a popup window
+        window.open(
+            "https://docs.google.com/document/d/1qPbP5rT9Azi8CMUa8ZeLvxgr_Nw907LhM239D2zolsE/edit?usp=sharing",
+            "_blank",
+            "width=600,height=600"
+        );
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (termsChecked) {
+            console.log("Form submitted!");
+            // Add your form submission logic here
+        } else {
+            alert("Please accept the terms and services before submitting.");
+        }
+    };
     return (<>
         <div className={"titleContainer"}>
             <div>Godle</div>
@@ -19,12 +40,28 @@ const Home = (props) => {
             <Slider title="Squeamishness"/>
             <Slider title="Technology"/>
             <Slider title="Erudition"/>
-            <Slider title="Organization "/>
+            <Slider title="Disposition"/>
             <Slider title="Morality"/>
             <Slider title="Zen"/>
             <Slider title="Aggression"/>
             <Slider title="Grandeur"/>
             <Slider title="Temperament"/>
+        </div>
+        <div className="terms-container">
+            <label>
+                <input
+                    type="checkbox"
+                    checked={termsChecked}
+                    onChange={handleTermsChange}
+                />
+                I accept the{" "}
+                <span
+                    onClick={handleTermsClick}
+                    style={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                    Terms and Services
+                </span>
+            </label>
         </div>
         <button type="submit" className="submit-button">Submit</button>
         </form>
