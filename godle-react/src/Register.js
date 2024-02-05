226cfe6e6f5163d5958f0744093d3da666d8acd8
Saaed Darwish <saaeddarwish1@gmail.com>
2024-02-05 14:27:03 -0700

    progress on register

--- godle-react/src/Register.js
+++ godle-react/src/Register.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
 const Register = () => {
+
+    const handleRegister = () => {
+        alert("example register");
+    }
+
     return (
         <div className="register-form">
-            <form action="/submit-your-form-endpoint" method="post">
-                <div>
-                    <label for="username">Username:</label>
+            <form onSubmit={handleRegister} method="post">
+                <div className="register-input">
+                    <label for="username">Username: &nbsp;</label>
                     <input type="text" id="username" name="username" required />
                 </div>
-                <div>
-                    <label for="email">Email:</label>
+                <div className="register-input">
+                    <label for="email">Email: &nbsp;</label>
                     <input type="email" id="email" name="email" required />
                 </div>
-                <div>
-                    <label for="password">Password:</label>
+                <div className="register-input">
+                    <label for="password">Password: &nbsp;</label>
                     <input type="password" id="password" name="password" required />
                 </div>
-                <button type="submit">Register</button>
+                <button className="register-button" type="submit">Register</button>
             </form>
-            <p>test</p>
         </div>
     )
 }
