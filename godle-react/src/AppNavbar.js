dc8f418ca2e50ec657d7733a0b73c0567a993149
jenniferja <jenniferjaystar2@gmail.com>
2024-03-16 19:47:32 -0600

    cleaned up code

--- godle-react/src/AppNavbar.js
+++ godle-react/src/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, ButtonGroup, Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Link, useNavigate } from 'react-router-dom';
 
