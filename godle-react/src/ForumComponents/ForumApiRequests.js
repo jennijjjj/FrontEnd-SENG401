8a2fdd2e003528c77b9befd02b8f82eb05dabb18
jenniferja <jenniferjaystar2@gmail.com>
2024-03-16 18:24:03 -0600

    Update ForumApiRequests.js

--- godle-react/src/ForumComponents/ForumApiRequests.js
+++ godle-react/src/ForumComponents/ForumApiRequests.js
@@ -16,3 +16,28 @@ export const fetchThreads = async (email) => {
 
     }});
 };
+
+export const addThread = async (email, body, title) => {
+    const userData = {
+        email: email,
+        title: title,
+        body: body,
+      };
+
+    fetch(`/Forum/postThread`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+          response.json().then(data => {
+            // Access the data in the response body
+            console.log(data);
+          });
+    }else{
+        console.error(response);
+
+    }});
+};
