d876309a18231bfecf58310805569d34315288af
celinahy <113138309+celinahy@users.noreply.github.com>
2024-03-23 20:24:24 -0600

    added forum functionality(forum card, parsing get request result, adding forum to controller)

--- godle-react/src/AdminComponents/SearchList.js
+++ godle-react/src/AdminComponents/SearchList.js
@@ -1,5 +1,6 @@
 import DeityCard from "./DeityCard";
 import UserCard from './UserCard'; 
+import ForumCard from './ForumCard';
 
 function SearchList({ filteredSearch, controller, fetchJsonData}) {
   let filtered;
@@ -8,6 +9,8 @@ function SearchList({ filteredSearch, controller, fetchJsonData}) {
     filtered = filteredSearch.map((deity) => <DeityCard deity={deity} fetchJsonData={fetchJsonData}/>);
   } else if (controller === "User") {
     filtered = filteredSearch.map((user) => <UserCard user={user} fetchJsonData={fetchJsonData}/>);
+  } else if (controller === "Forum") {
+    filtered = filteredSearch.map((forum) => <ForumCard forum={forum} fetchJsonData={fetchJsonData}/>);
   } else {
     filtered = null; // or handle other cases as needed
   }
