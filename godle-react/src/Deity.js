6083277bb9260d40db76f3698986fb3feb803e67
Saaed Darwish <saaeddarwish1@gmail.com>
2024-03-24 04:27:43 -0600

    OKAY deity page should be fixed on all screens

--- godle-react/src/Deity.js
+++ godle-react/src/Deity.js
@@ -24,28 +24,30 @@ const Deity = ({ deity }) => {
     return (
         <>
             {deity ? (
-                <div className="deity-profile">
-                    <img className="deity-profile-image" src={"./images/" + deity.imagePath} alt={deity.name} />
-                    <p className="deity-profile-name">{deity.name}</p>
-                    <p className="deity-profile-source-universe">{deity.sourceUniverse}</p>
-                    <div className="deity-profile-description-attributes-container">
-                        <div className="deity-profile-attributes">
-                            <p style={{ textAlign: "right", marginRight: "168px", paddingTop: "122px" }}>HIGH</p>
-                            <p style={{ marginRight: "154px", paddingTop: "46px" }}>Attributes</p>
-                            {highestAttributes.map(([attribute, value]) => (
-                                <p style={{ color: "#FFFFFF", paddingRight: "30px" }} key={attribute}>{attribute}: {value}</p>
-                            ))}
-                        </div>
-                        <div className="deity-profile-description">
-                            <h2 className="deity-profile-description-title">DESCRIPTION</h2>
-                            <p>{deity.description}</p>
-                        </div>
-                        <div className="deity-profile-attributes">
-                            <p style={{ textAlign: "left", marginLeft: "168px", paddingTop: "122px" }}>LOW</p>
-                            <p style={{ marginLeft: "154px", paddingTop: "46px" }}>Attributes</p>
-                            {lowestAttributes.map(([attribute, value]) => (
-                                <p style={{ color: "#FFFFFF", paddingLeft: "30px" }} key={attribute}>{attribute}: {value}</p>
-                            ))}
+                <div className="deity-container">
+                    <div className="deity-profile">
+                        <img className="deity-profile-image" src={"./images/" + deity.imagePath} alt={deity.name} />
+                        <p className="deity-profile-name">{deity.name}</p>
+                        <p className="deity-profile-source-universe">{deity.sourceUniverse}</p>
+                        <div className="deity-profile-description-attributes-container">
+                            <div className="deity-profile-attributes">
+                                <p style={{ textAlign: "right", marginRight: "168px", paddingTop: "122px" }}>LOW</p>
+                                <p style={{ marginRight: "154px", paddingTop: "46px" }}>Attributes</p>
+                                {lowestAttributes.map(([attribute, value]) => (
+                                    <p style={{ color: "#FFFFFF", paddingRight: "30px" }} key={attribute}>{attribute}: {value}</p>
+                                ))}
+                            </div>
+                            <div className="deity-profile-description">
+                                <h2 className="deity-profile-description-title">DESCRIPTION</h2>
+                                <p>{deity.description}</p>
+                            </div>
+                            <div className="deity-profile-attributes">
+                                <p style={{ textAlign: "left", marginLeft: "168px", paddingTop: "122px" }}>HIGH</p>
+                                <p style={{ marginLeft: "154px", paddingTop: "46px" }}>Attributes</p>
+                                {highestAttributes.map(([attribute, value]) => (
+                                    <p style={{ color: "#FFFFFF", paddingLeft: "30px" }} key={attribute}>{attribute}: {value}</p>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
