68881e06ae27615d0cebafc42fc6054f15e91ace
celinahy <113138309+celinahy@users.noreply.github.com>
2024-03-23 20:44:23 -0600

    delete forum works correctly

--- godle-react/src/AdminComponents/ApiRequests/DeleteRequests.js
+++ godle-react/src/AdminComponents/ApiRequests/DeleteRequests.js
@@ -68,3 +68,39 @@ export const deleteUser = async (Email) => {
         throw error; // Re-throw the error for the caller to handle
     }
 };
+
+export const deleteForum = async (Email, Date) => {
+    const data ={
+        Email: Email,  
+        Date: Date
+    }
+
+    try {
+        fetch('/Admin/Forum', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+            })
+            .then(response => {
+                if (!response.ok) {
+                    console.log(data);
+                    alert(`ERROR: could not delete forum from ${Email} with postdate of ${Date}.`);
+                throw new Error('Network response was not ok');
+                }
+                alert(`Successfully deleted forum from ${Email} with postdate of ${Date}.`);
+                return response.json();
+            })
+            .then(data => {
+                console.log('DELETE request successful');
+                console.log(data); // Response data from the server
+            })
+            .catch(error => {
+                console.error('Error during DELETE request:', error);
+            });
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        throw error; // Re-throw the error for the caller to handle
+    }
+};
